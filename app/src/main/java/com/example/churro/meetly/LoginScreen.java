56f533d468516c349d71af761094edb2b78d70f5
nurandhawa <noorr8@gmail.com>
2015-04-08 18:46:56 -0700

    - Added Server interface and impl class. Added library too

--- app/src/main/java/com/example/churro/meetly/LoginScreen.java
+++ app/src/main/java/com/example/churro/meetly/LoginScreen.java
@@ -1,9 +1,14 @@
 package com.example.churro.meetly;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 
@@ -16,10 +21,41 @@ public class LoginScreen extends ActionBarActivity {
 
 
         EditText un = (EditText) findViewById(R.id.tf_username);
-        String username = un.toString();
+        final String username = un.toString();
 
         EditText pd = (EditText) findViewById(R.id.tf_pwd);
-        String pwd = pd.toString();
+        final String pwd = pd.toString();
+
+        Button login = (Button) findViewById(R.id.btn_login1);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MeetlyServerImpl server = new MeetlyServerImpl();
+
+                try {
+                    int token = server.login(username, pwd);
+                } catch (MeetlyServer.FailedLoginException e) {
+
+                    dialogPopUp();
+                }
+                LoginScreen.this.finish();
+
+            }
+        });
     }
+
+    private void dialogPopUp() {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Invalid User Information");
+        alertDialog.setMessage("Invalid password or username. Please try again!");
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        alertDialog.show();
+    }
+
 
 }
